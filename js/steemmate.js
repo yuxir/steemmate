9ec927b5021070756faea0ce86b5fa1d7cca8146
yuxi <me@yuxifan.com>
2018-06-27 00:21:25 +0100

    get jquery selector working

--- js/steemmate.js
+++ js/steemmate.js
@@ -1,2 +1,7 @@
 'use strict';
 
+async function updateui() {
+  $( '#content div div.App__content div.UserProfile div div.row:eq(1)' ).prepend( '<div style="padding-top:1em; float:left; width:30%"><h4>Steem mate</h4></div>');
+}
+
+updateui();
