69a994e50646d8dd57416e92697b324560592912
yuxi <me@yuxifan.com>
2018-06-29 00:08:00 +0100

    find username from URL and add DIV to UI

--- js/steemmate.js
+++ js/steemmate.js
@@ -1,7 +1,15 @@
 'use strict';
 
 async function updateui() {
-  $( '#content div div.App__content div.UserProfile div div.row:eq(1)' ).prepend( '<div style="padding-top:1em; float:left; width:30%"><h4>Steem mate</h4></div>');
+	let pattern = /@[^\/]*/;
+	let username_array = window.location.href.match(pattern);
+	
+	if(username_array) {
+        let username = username_array[0];
+        let html     = '<div style="padding-top:1em; float:left; width:30%"><h4>Steem mate ['+username+']</h4></div>';
+        $( '#content div div.App__content div.UserProfile div div.row:eq(1)' ).prepend( html );
+	}
+	
 }
 
 updateui();
