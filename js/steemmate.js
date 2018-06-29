2fdae56cbf21a48b0fc6c05a474ec60295a98c4f
yuxi <me@yuxifan.com>
2018-06-29 21:09:16 +0100

    read user history from steem blockchain

--- js/steemmate.js
+++ js/steemmate.js
@@ -41,13 +41,49 @@ function operation_icons(operation) {
     return '';
 }
 
+/*
+ * find numbers of latest record from user's account history
+ */
+async function find_last_record(user, numbers) {
+  let result;
+
+  try{
+    result = await steem.api.getAccountHistoryAsync(user, Number.MAX_SAFE_INTEGER, numbers);
+  }catch(e){
+    console.log(e);
+  }
+
+  return result;
+}
+
+// Update page
 async function updateui() {
 	let pattern = /@[^\/]*/;
 	let username_array = window.location.href.match(pattern);
 	
 	if(username_array) {
-        let username = username_array[0];
-        let html     = '<div style="padding-top:1em; float:left; width:30%"><h4>Steem mate ['+username+']</h4></div>';
+		let username = username_array[0];
+		
+    	let numbers = 100;  // use a fixed number for now, will be configurable later
+		
+		// get numbers of records from user account history
+	    let result  = await find_last_record(username, numbers);
+		let html    = '<div style="padding-top:1em; float:left; width:30%"><h4>Steem mate ['+username+']</h4></div>';
+
+		// construct html output
+        for(let r in result.reverse()) {
+            let trx_id         = result[r][1]['trx_id'];
+            let block          = result[r][1]['block'];
+            let trx_in_block   = result[r][1]['trx_in_block'];
+            let op_in_trx      = result[r][1]['op_in_trx'];
+            let virtual_op     = result[r][1]['virtual_op'];
+            let timestamp      = result[r][1]['timestamp'];
+            let op             = result[r][1]['op'];
+
+            html = html + '<p>' + timestamp + ' ' + operation_html(op) + '</p>';
+        }
+
+		// Add a div to show the additional info
         $( '#content div div.App__content div.UserProfile div div.row:eq(1)' ).prepend( html );
 	}
 	
