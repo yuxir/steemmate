03b5bb6808b2e670316334f3bb41aed451c0b441
yuxi <me@yuxifan.com>
2018-07-01 10:05:55 +0100

    add default popup page

---
+++ js/ui.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// when content loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // init tabs
+    $(function() {
+        $( "#tabs" ).tabs();
+    });
+}, false);
